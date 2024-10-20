552454dc62cba4aefe26fc522d98f2827a54b28b
isBeo <dammybeo@gmail.com>
2024-10-20 15:21:02 -0700

    hgjgjh

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Hero =()=>{
   const w = '700'
-  const h = '400'
+  const h = '350'
   return (
     <div>
       <img src={`https://media.istockphoto.com/id/172664772/nl/foto/house-plans.jpg?s=1024x1024&w=is&k=20&c=QM346QJZc5uXdw6S98RVufLrBkbFKisaaQGhJTPK5dU=`} 
