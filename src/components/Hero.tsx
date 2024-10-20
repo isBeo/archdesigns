374490513fe6c43451731a65a6b12681345d2c64
isBeo <dammybeo@gmail.com>
2024-10-20 15:15:40 -0700

    some changes to the nav and hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 
 const Hero =()=>{
-  const w = '500'
-  const h = '500'
+  const w = '700'
+  const h = '400'
   return (
     <div>
       <img src={`https://media.istockphoto.com/id/172664772/nl/foto/house-plans.jpg?s=1024x1024&w=is&k=20&c=QM346QJZc5uXdw6S98RVufLrBkbFKisaaQGhJTPK5dU=`} 
