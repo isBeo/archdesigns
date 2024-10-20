708c9079018efaa05cffc56e4bdde12cd5b4941e
isBeo <dammybeo@gmail.com>
2024-10-20 15:59:09 -0700

    some more edits

---
+++ src/components/FloCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const FloCard = () => {
+  return (
+    <div 
+       className='
+          shadow-sm shadow-slate-900
+          w-full  bg-slate-200 p-3 lg:hidden
+    '>
+        Architectural Designs made just for you <br />
+        Let's design a house plan that suits your life style.
+
+        <button>
+            Discover Now
+        </button>
+    </div>
+  )
+}
+
+export default FloCard
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,26 @@
-import Image from "next/image"
+import FloCard from "./FloCard"
+
+
 
 const Hero =()=>{
   const w = '700'
   const h = '350'
   return (
-    <div>
-      <img src={`https://media.istockphoto.com/id/172664772/nl/foto/house-plans.jpg?s=1024x1024&w=is&k=20&c=QM346QJZc5uXdw6S98RVufLrBkbFKisaaQGhJTPK5dU=`} 
+    <div className="relative mb-10">
+
+     <div className="img max-w-[780] ">
+     <img src={`https://media.istockphoto.com/id/172664772/nl/foto/house-plans.jpg?s=1024x1024&w=is&k=20&c=QM346QJZc5uXdw6S98RVufLrBkbFKisaaQGhJTPK5dU=`} 
         alt="hero" width={w}  
         height={h}
       />
+     </div>
+
+     <div className="floatingCard absolute
+        -bottom-4 max-w-[300px] ml-3 
+        
+        ">
+        <FloCard />
+     </div>
     </div>
   )
 }
